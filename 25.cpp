104b1f5f5723537f9f903ebb9b7e4b4d4c572329
Harsh Sharma <harshsharma41664@gmail.com>
2022-03-11 04:40:43 +0530

    tired

--- 25.cpp
+++ 25.cpp
@@ -1,4 +1,42 @@
-\//
 // Created by harsh on 01/03/22.
 //
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
+struct Student
+{
+    int rollNum{};
+    std::string name{};
+    std::string Class{};
+    int year{};
+    int Tmarks{};
+};
+
+int main()
+{
+    Student S[3];
+    std::ofstream fout;
+    fout.open("StudentData.txt");
+
+    for (int i{1};i<=3;++i)
+    {
+        std::cout<<"\nStudent "<<i<<'\n';
+        std::cout<<"Enter Rollnum: ";
+        std::cin>>S[i].rollNum;
+        std::cout<<"Enter Name: ";
+        std::cin>>S[i].name;
+        std::cout<<"Enter Class: ";
+        std::cin>>S[i].Class;
+        std::cout<<"Enter the Year: ";
+        std::cin>>S[i].year;
+        std::cout<<"Enter the Total Marks: ";
+        std::cin>>S[i].Tmarks;
+        fout<<S[i].rollNum<<" "<<S[i].name<<" "<<S[i].Class<<" "<<S[i].year
+        <<" "<<S[i].Tmarks;
+    }
+    fout.close();
+
+    return 0;
+}
