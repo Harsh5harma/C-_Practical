90c0159d5ec680187635889af6060e026e096a05
Harsh Sharma <harshsharma41664@gmail.com>
2022-03-11 15:49:11 +0530

    done

--- 27.cpp
+++ 27.cpp
@@ -1,4 +1,32 @@
-//
 // Created by harsh on 01/03/22.
 //
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    std::ifstream Fin{};
+    std::ofstream Fout{};
+
+    Fin.open("Student_Data.txt");
+    Fout.open("DataWithoutSpaces.txt");
+
+    std::string str{};
+
+    while (std::getline(Fin,str))
+    {
+        for (int i{0};i<str.length();++i)
+        {
+            if (str[i]!=' ')
+                Fout<<str[i];
+        }
+        Fout<<'\n';
+    }
+    Fin.close();
+    Fout.close();
+
+    return 0;
+}
 
