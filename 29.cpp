73f7edcd5504c1ee64e34cfc1c32e11f8e8856b4
Harsh Sharma <harshsharma41664@gmail.com>
2022-03-11 15:44:25 +0530

    done

--- 29.cpp
+++ 29.cpp
@@ -1,4 +1,54 @@
-//
 // Created by harsh on 01/03/22.
-//
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Enter 10 array elements: ";
+    int array[10];
+    for (int i{0};i<10;++i)
+        std::cin>>*(array+i);
+
+    std::cout<<"The array you entered is: ";
+    for (int i{0};i<10;++i)
+        std::cout<<*(array+i)<<' ';
+
+    int* ptr = array;
+    for (int i{0};i<10;++i)
+    {
+        for (int j{i+1};j<10;++j)
+        {
+            if(*(ptr+i)>*(ptr+j))
+            {
+                int Temp = *(ptr+i);
+                *(ptr+i) = *(ptr+j);
+                *(ptr+j) = Temp;
+            }
+        }
+    }
+
+    std::cout<<"\nArray in ascending order: ";
+    for (int i{0};i<10;++i)
+        std::cout<<*(array+i)<<' ';
+
+    for (int i{0};i<10;++i)
+    {
+        for (int j{i+1};j<10;++j)
+        {
+            if(*(ptr+i)<*(ptr+j))
+            {
+                int Temp = *(ptr+i);
+                *(ptr+i) = *(ptr+j);
+                *(ptr+j) = Temp;
+            }
+        }
+    }
+    std::cout<<"\nArray in descending order: ";
+    for (int i{0};i<10;++i)
+        std::cout<<*(array+i)<<' ';
+    return 0;
+
+
+    return 0;
+}
 
