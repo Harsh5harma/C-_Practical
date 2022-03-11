079b59588cdc39b1e2be564981ceffae1d6f9f21
Harsh Sharma <harshsharma41664@gmail.com>
2022-03-11 15:56:56 +0530

    done

--- 15.cpp
+++ 15.cpp
@@ -1,4 +1,27 @@
-//
 // Created by harsh on 01/03/22.
 //
+
+#include <iostream>
+
+int main()
+{
+    int n{},sum{};
+    std::cout<<"Enter the number of elements: ";
+    std::cin>>n;
+
+    int *arr = new int[n];
+
+    std::cout<<"Enter the elements: ";
+    for (int i{0};i<n;++i)
+    {
+        int x{};
+        std::cin>>x;
+        arr[i]=x;
+        sum+=x;
+    }
+
+    std::cout<<"The sum of the elements is: "<<sum;
+    delete[] arr;
+    return 0;
+}
 
